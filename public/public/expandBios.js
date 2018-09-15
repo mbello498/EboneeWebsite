dcd67b0c301880ca25901a25a61cf97b80d4b82b
Maya Bello <mbello@ucsd.edu>
2018-09-15 09:21:02 -0700

    First commit, website template complete

---
+++ public/public/expandBios.js
@@ -0,0 +1,97 @@
+function seeMoreTextEd() {
+    var x = document.getElementById("ed");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextAl() {
+    var x = document.getElementById("al");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function seeMoreTextYohance() {
+    var x = document.getElementById("yohance");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextAndrea() {
+    var x = document.getElementById("andrea");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextAaron() {
+    var x = document.getElementById("aaron");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextDorian() {
+    var x = document.getElementById("dorian");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextChris() {
+    var x = document.getElementById("chris");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextJack() {
+    var x = document.getElementById("jack");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextRashida() {
+    var x = document.getElementById("rashida");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextJene() {
+    var x = document.getElementById("jane");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextEbonee() {
+    var x = document.getElementById("ebonee");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function seeMoreTextCalvin() {
+    var x = document.getElementById("calvin");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
